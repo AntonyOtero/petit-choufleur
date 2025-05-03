d429fe1ecb90042c1e3ec9365cd14b363a546a9b
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-05-03 17:45:16 -0400

    feature: Click a nav button to render its respective tab

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@ import { home } from "./components/home/home";
 import "./styles.css";
 
 const Content = document.querySelector("#content");
+const NavButtons = document.querySelectorAll(".nav-tab");
+
+const components = {
+    home: home(),
+}
+
+NavButtons.forEach(button => {
+    button.addEventListener("click", e => {
+        const ActiveTab = document.querySelector(".active");
+        if (ActiveTab) ActiveTab.classList.remove("active");
+        e.target.classList.add("active");
+        Content.innerHTML = "";
+        Content.append(components[e.target.dataset.tab]);
+    });
+});
 
 Content.append(home());
